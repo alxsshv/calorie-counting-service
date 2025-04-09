d01e68a7705e5e35bf510508202a50e57e2e00f8
aleksei_shvariov <alxsshv@gmail.com>
2025-04-09 16:56:15 +0300

    Добавлены классы для работы с сущностью "Прием пищи".

--- src/main/java/com/alxsshv/dto/FoodIntakeDto.java
+++ src/main/java/com/alxsshv/dto/FoodIntakeDto.java
@@ -1,9 +1,7 @@
 package com.alxsshv.dto;
 
-import com.alxsshv.model.ServingSize;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
